01f702fd670502d9569f07f56c033747dcb21d11
Alvin Wilbert <awilbert@student.ciputra.ac.id>
2021-12-24 19:07:16 +0800

    Add project files.

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Player = new System.Windows.Forms.PictureBox();
+            this.gametimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Player
+            // 
+            this.Player.BackColor = System.Drawing.Color.DimGray;
+            this.Player.Location = new System.Drawing.Point(360, 450);
+            this.Player.Name = "Player";
+            this.Player.Size = new System.Drawing.Size(80, 100);
+            this.Player.TabIndex = 0;
+            this.Player.TabStop = false;
+            // 
+            // gametimer
+            // 
+            this.gametimer.Enabled = true;
+            this.gametimer.Tick += new System.EventHandler(this.gametimerevent);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(782, 553);
+            this.Controls.Add(this.Player);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keydownevent);
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Player;
+        private System.Windows.Forms.Timer gametimer;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        int vertikal = 0;
+        int horizontal = 0;
+        int pergerakanHorizontal = 60;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void keydownevent(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up && Player.Top > 300)
+            {
+                vertikal = -100;
+            }
+            if (e.KeyCode == Keys.Down && Player.Top < 300)
+            {
+                vertikal = 100;
+            }
+            if (e.KeyCode == Keys.Left && Player.Left > 60)
+            {
+                horizontal = -60;
+            }
+            if (e.KeyCode == Keys.Right && Player.Left <= 480)
+            {
+                horizontal = 60;
+            }
+        }
+        private void gametimerevent(object sender, EventArgs e)
+        {
+            Player.Top += vertikal;
+            Player.Left += horizontal;
+            vertikal = 0;
+            horizontal = 0;
+        }
+    }
+}
